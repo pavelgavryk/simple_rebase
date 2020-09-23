e888efafcd8bf01f4b6b6baabf82a8830b891f3e
Pavel Gavryk <gavryk.pavel@gmail.com>
2020-09-23 12:24:44 +0300

    implement subtract-operation handler

---
+++ src/com/gmail/gavryk/SubtractOperationHandler.java
@@ -0,0 +1,10 @@
+package com.gmail.gavryk;
+
+/**
+ * Date: 23.09.2020
+ *
+ * @author Pavel Gavryk
+ */
+public interface SubtractOperationHandler {
+	int subtract(int a, int b);
+}
---
+++ src/com/gmail/gavryk/impl/SubtractOperationHandlerImpl.java
@@ -0,0 +1,16 @@
+package com.gmail.gavryk.impl;
+
+import com.gmail.gavryk.SubtractOperationHandler;
+
+/**
+ * Date: 23.09.2020
+ *
+ * @author Pavel Gavryk
+ */
+public class SubtractOperationHandlerImpl implements SubtractOperationHandler {
+
+	@Override
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+}
