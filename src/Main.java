ed53d37bdb7a790ff93162e6b18f643d5d18e288
Pavel Gavryk <gavryk.pavel@gmail.com>
2020-09-23 11:55:42 +0300

    add calculator interface

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,5 @@
  */
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hi");
-		System.out.println("secondCommit");
-		System.out.println("additional branch created");
 	}
 }
---
+++ src/com/gmail/gavryk/Calculator.java
@@ -0,0 +1,10 @@
+package com.gmail.gavryk;
+
+/**
+ * Date: 23.09.2020
+ *
+ * @author Pavel Gavryk
+ */
+public interface Calculator {
+	int add(int a, int b);
+}
